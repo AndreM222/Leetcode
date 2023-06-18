2d347568a3290cba4ca482788e6a5e04c7d02e51
Andre Mossi <98118238+AndreM222@users.noreply.github.com>
2023-06-17 21:12:00 -0600

    feat(add thrid solution to 4): add another solution based from solution 1 that speed gets increased
    use numbers instead of deleting vector slots to keep track of sorting

--- Hard-Level/CPP-Solutions/Median-of-Two-Sorted-Arrays.cpp
+++ Hard-Level/CPP-Solutions/Median-of-Two-Sorted-Arrays.cpp
@@ -3,7 +3,7 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int mid;
-        vector<int> list;
+        vector<int> list; // Temporary List
 
         mid = (nums1.size() + nums2.size()) / 2; // Find Mid Point
 
@@ -36,10 +36,10 @@ public:
             nums2.erase(nums2.begin()); // Erase first element added
         }
 
-        // if greater then 1 and total is odd then
+        // if odd then return middle
         if (list.size() % 2) return list[mid];
 
-        // Else return average of the two middle ones
+        // else return average of the two middle ones
         return (double)(list[mid] + list[mid - 1]) / 2;
     }
 };
@@ -64,5 +64,56 @@ public:
         
         // Else return average of the two middle ones
         return (double)(list[mid - 1] + list[mid]) / 2;
+    }
+};
+
+// Solution 3: Same as solution 1 but use variables for counter instead of delete elements from the vectors
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int mid, l1, l2, m1, m2;
+        vector<int> list; // Temporary List
+
+        l1 = l2 = 0; // Current Index
+
+        m1 = nums1.size(); // Max loop
+        m2 = nums2.size(); // Max loop
+
+        mid = (m1 + m2) / 2;
+
+        // Sort the two vectors untill one is empty
+        while (m1 > l1 && m2 > l2)
+        {
+            if(nums1[l1] < nums2[l2])
+            {
+                list.push_back(nums1[l1]); // Add element from index
+                l1++;
+            }
+            else
+            {
+                list.push_back(nums2[l2]); // Add element from index
+                l2++;
+            }
+        }
+
+        // Sort vector 1 remaining ones
+        while (m1 > l1)
+        {
+            list.push_back(nums1[l1]); // Add element from index
+            l1++;
+        }
+
+        // Sort vector 2 remaining ones
+        while (m2 > l2)
+        {
+            list.push_back(nums2[l2]); // Add element from index
+            l2++;
+        }
+
+        // if odd then return middle
+        if (list.size() % 2) return list[mid];
+
+        // if odd then return middle
+        return (double)(list[mid] + list[mid - 1]) / 2;
     }
 };
