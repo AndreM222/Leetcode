ffd712aa8d80f51d42e67929f12acb2418b5ecfe
Andre Mossi <98118238+AndreM222@users.noreply.github.com>
2024-02-06 12:38:55 -0500

    feat(comments): longest Common Subsequence

--- Medium-Level/CPP-Solutions/Longest-Common-Subsequence.cpp
+++ Medium-Level/CPP-Solutions/Longest-Common-Subsequence.cpp
@@ -4,20 +4,21 @@ class Solution
 public:
     int longestCommonSubsequence(string text1, string text2)
     {
-        return lcsRecursion(text1, text2);
+        return lcsRecursion(text1, text2); // Start Recursion
     }
 
 private:
     int lcsRecursion(string text1, string text2, int pos1 = 0, int pos2 = 0)
     {
-        if(text1.length() == pos1 || text2.length() == pos2) return 0;
-        else if(text1[pos1] == text2[pos2]) return lcsRecursion(text1, text2, pos1 + 1, pos2 + 1) + 1;
-        else 
+        if(text1.length() == pos1 || text2.length() == pos2) return 0; // if end of min array then return 0
+        // If current characters are equal then increase by 1
+        else if(text1[pos1] == text2[pos2]) return lcsRecursion(text1, text2, pos1 + 1, pos2 + 1) + 1; 
+        else
         {
-            int left = lcsRecursion(text1, text2, pos1 + 1, pos2);
-            int right = lcsRecursion(text1, text2, pos1, pos2 + 1);
+            int left = lcsRecursion(text1, text2, pos1 + 1, pos2); // return total sequence starting with next letter of array 1
+            int right = lcsRecursion(text1, text2, pos1, pos2 + 1); // return total sequence starting with next letter of array 2
 
-            return left > right ? left : right;
+            return left > right ? left : right; // return greatest sequence
         }
     }
 };
@@ -45,12 +46,13 @@ public:
         // Parse through the slots words to find the common subsequence
         for (int i = 0; i < length1; ++i) {
             for (int j = 0; j < length2; ++j) {
-                if (text1[i] == text2[j]) {
+                if (text1[i] == text2[j]) { // If equal than increase by 1
                     total[i + 1][j + 1] = total[i][j] + 1;
                 }
 
                 else {
-                    total[i + 1][j + 1] = total[i][j + 1] > total[i + 1][j] ? total[i][j + 1] : total[i + 1][j];
+                    // else compare with and set the greatest of the sequences
+                    total[i + 1][j + 1] = total[i][j + 1] > total[i + 1][j] ? total[i][j + 1] : total[i + 1][j]; 
                 }
             }
         }
