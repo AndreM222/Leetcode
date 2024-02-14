cfef85a21015a287b161c7264ef7d99938c998f1
Andre Mossi <98118238+AndreM222@users.noreply.github.com>
2024-02-14 13:04:21 -0500

    refactor(7): reversed integer comment

--- Medium-Level/CPP-Solutions/Reverse-Integer.cpp
+++ Medium-Level/CPP-Solutions/Reverse-Integer.cpp
@@ -5,11 +5,11 @@ public:
 
         while(x)
         {
-            reversed = (reversed * 10) + (x % 10);
-            x /= 10;
-            if(reversed > INT_MAX || reversed < INT_MIN) return 0;
+            reversed = (reversed * 10) + (x % 10); // Get last digit from right and add to the right of the reversed
+            x /= 10; // Remove last digit from right in the normal int
+            if(reversed > INT_MAX || reversed < INT_MIN) return 0; // Check it is in 32 bits range if not than return 0
         }
 
-        return reversed;
+        return reversed; // Return reversed
     }
 };
