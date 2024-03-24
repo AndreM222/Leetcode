858e8733c4a29378a9da38a0e853088d3e99d57a
Andre Mossi <mossiroberto0392@gmail.com>
2024-03-24 11:29:30 -0400

    docs(98): validate Binary Search Tree Doc

--- Medium-Level/CPP-Solutions/Validate-Binary-Search-Tree.cpp
+++ Medium-Level/CPP-Solutions/Validate-Binary-Search-Tree.cpp
@@ -13,19 +13,20 @@
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        listSetup(root);
-        for(int i = 0; i < list.size() - 1; i++)
+        listSetup(root); // Set list and organize tree
+        for(int i = 0; i < list.size() - 1; i++) // Parse through list
         {
-            if(list[i + 1] <= list[i]) return false;
+            if(list[i + 1] <= list[i]) return false; // If index is bigger or equal to next than return false
         }
-        return true;
+
+        return true; // return true if list is in order
     }
 private:
-    vector<int> list;
-    void listSetup(TreeNode* root)
+    vector<int> list; // Set list of binary tree
+    void listSetup(TreeNode* root) // Run in-order recursion to setup list
     {
-        if(root->left) listSetup(root->left);
-        list.push_back(root->val);
-        if(root->right) listSetup(root->right);
+        if(root->left) listSetup(root->left); // If left node available then traverse through it
+        list.push_back(root->val); // Push current node value to list
+        if(root->right) listSetup(root->right); // If right node available then traverse through it
     }
 };
